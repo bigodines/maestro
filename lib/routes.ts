a3a07b0efa4a6fb4d2998b08eee33e097b8d7cda
bigo <matheus.mendes@vungle.com>
2017-04-10 14:27:09 -0700

    stashed changes from a while ago

--- lib/routes.ts
+++ lib/routes.ts
@@ -6,7 +6,7 @@ import * as Router from 'koa-router';
 import {Request} from 'koa';
 import * as chalk from 'chalk';
 
-const router = new Router();
+let router = new Router();
 
 /**
  * Base route
@@ -18,12 +18,17 @@ router.get('/', async (ctx, next) => {
   await next();
 });
 
+router.all('proxy', /^\/(.*)(?:\/|$)/, async (ctx, next) => {
+  await next();
+});
+
 export default router;
 
-interface IKoaRequestWithBody extends Router.IRouterContext {
+/*interface IKoaRequestWithBody extends Router.IRouterContext {
   request: IKoaBodyParserRequest;
 }
 
 interface IKoaBodyParserRequest extends Request {
   body: any;
 }
+ */
